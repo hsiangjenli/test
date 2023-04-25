fbca81cb0e046864ad135bf61b8975d61a4dde62
Hsiang-Jen Li <71996166+hsiangjenli@users.noreply.github.com>
2023-04-25 14:34:26 +0800

    merge: Test/rebase 3 (#5)
    * feat: ast
    
    * feat: rebase-1
    
    * feat: rebase-2
    
    * docs: update CHANGELOG.md for v1.7.0 [skip ci]
    
    * feat: rebase-2
    
    * feat: rebase-3
    
    * feat: rebase-444444444
    
    * # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch feat/test-vscode-view
    # Changes to be committed:
    #    modified:   3.py
    #
    
    ---------
    
    Co-authored-by: hsiangjenli <hsiangjenli@users.noreply.github.com>

---
+++ run.sh
@@ -0,0 +1,4 @@
+# PYTHON=/mnt/c/Miniconda3/python.exe
+# "$PYTHON" demo-1.py
+echo "hello world - 1" | python demo-2.py
+echo "hello cat - 2" | python demo-2.py
---
+++ test.sh
@@ -0,0 +1,10 @@
+PYTHON=/mnt/c/Users/ASUS/.conda/envs/igs-2/python.exe
+# where python
+echo "================= BetNo.0Charge0" && echo "BetNo.0Charge0" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.0Charge1" && echo "BetNo.0Charge1" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.1Charge0" && echo "BetNo.1Charge0" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.1Charge1" && echo "BetNo.1Charge1" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.2Charge0" && echo "BetNo.2Charge0" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.2Charge1" && echo "BetNo.2Charge1" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.3Charge0" && echo "BetNo.3Charge0" | "$PYTHON" Refactor/igs.py
+echo "================= BetNo.3Charge1" && echo "BetNo.3Charge1" | "$PYTHON" Refactor/igs.py
